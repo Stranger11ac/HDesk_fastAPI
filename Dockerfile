d406fef681f3b6c90a71d018a0c3c8b8c5c13b77
Stranger11ac <106894827+Stranger11ac@users.noreply.github.com>
2026-04-25 00:20:50 -0600

    Change variables and app docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.11-slim
+WORKDIR /app
 
 # Copia primero dependencias (cache)
 COPY main.txt .
--- database.py
+++ database.py
@@ -4,6 +4,9 @@ from settings import settings
 
 def get_connection():
     print("✅ Connecting to database...")
+    print("DB_HOST:", settings.db_host)
+    print("DB_NAME:", settings.db_name)
+
     return psycopg2.connect(
         host=settings.db_host,
         user=settings.db_user,
